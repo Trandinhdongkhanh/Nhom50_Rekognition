cba8ae44e63d22481b7d13741abfc772cf71c372
Daongocthach <thach752002@gmail.com>
2022-12-05 23:55:43 +0700

    update function and client

--- application.py
+++ application.py
@@ -63,3 +63,103 @@ class App():
 
         self.btn_upload.place(x=650, y=160)
         self.st.place(x=430, y=20, width=PIC_WIDTH, height=PIC_HEIGHT)
+
+    def upload_img(self):
+        global file
+        file = FileBrowser.browseFile()
+        if file == '':        #If browseFile() return an empty string on cancel
+            return
+        img = Image.open(file).resize((PIC_WIDTH, PIC_HEIGHT), Image.ANTIALIAS)
+        input_img = ImageTk.PhotoImage(img)
+        self.lbl_Pic.configure(image=input_img)
+        self.lbl_Pic.image = input_img
+        self.display_btn()
+        self.reset()
+
+    def display_btn(self):
+        self.btn_analyze.place(x=500, y=220)
+        self.btn_detectLabels.place(x=600, y=220)
+        self.btn_detectText.place(x=700, y=220)
+        self.btn_facialAnalysis.place(x=800, y=220)
+
+    def reset(self):
+        self.lbl_resultPic.configure(image=None)
+        self.lbl_resultPic.image = None
+        self.st.configure(state='normal')
+        self.st.delete(1.0, tk.END)
+        self.st.configure(state='disabled')
+
+
+    def detect_celebs(self):
+        self.st.configure(state='normal')
+        rekAPI = Rekognition(CLIENT)
+        result = rekAPI.recognize_celebrities_v2(S3_RESOURCE, BUCKET, file)
+        if(len(result) == 0):
+            print("No celeb detected !")
+            self.st.insert(tk.INSERT, "No celeb detected !")
+            self.st.configure(state='disabled')
+            return
+        print(result)
+        print()
+        for obj in result:
+            face_id = rekAPI.search_faces_by_image(COLLECTION_ID, BUCKET, obj)
+            celeb = DynamoDB.query(TABLE, PARTITION_KEY, face_id)
+            ID = 'ID: ' + celeb[0]['face_id'] + '\n'
+            Gender = 'KnownGender: ' + celeb[0]['KnownGender'] + '\n'
+            Info = 'Info: ' + celeb[0]['Info'] + '\n'
+            Name = 'Name: ' + celeb[0]['Name'] + '\n'
+            self.st.insert(tk.INSERT, Name)
+            self.st.insert(tk.INSERT, ID)
+            self.st.insert(tk.INSERT, Gender)
+            self.st.insert(tk.INSERT, Info)
+            self.st.insert(tk.INSERT, '\n')
+            print('\nResult from DynamoDB: ')
+            print(celeb)
+            print('------------------------------------')
+
+        self.st.configure(state='disabled')
+        self.copy_img()
+
+    def detect_labels(self):
+        self.st.configure(state='normal')
+        rekAPI = Rekognition(CLIENT)
+        result = rekAPI.detect_labels(file)
+        self.st.insert(tk.INSERT, result)
+        self.st.configure(state='disabled')
+        self.copy_img()
+
+    def detect_text(self):
+        self.st.configure(state='normal')
+        rekAPI = Rekognition(CLIENT)
+        result = rekAPI.detect_text(file)
+        self.st.insert(tk.INSERT, result)
+        self.st.configure(state='disabled')
+        self.copy_img()
+
+    def facial_analysis(self):
+        self.st.configure(state='normal')
+        rekAPI = Rekognition(CLIENT)
+        result = rekAPI.facial_analysis(file)
+        self.st.insert(tk.INSERT, result)
+        self.st.configure(state='disabled')
+        self.copy_img()
+
+    def copy_img(self):
+        img = Image.open(file).resize((PIC_WIDTH, PIC_HEIGHT), Image.ANTIALIAS)
+        output_img = ImageTk.PhotoImage(img)
+        self.lbl_resultPic.configure(image=output_img)
+        self.lbl_resultPic.image = output_img
+
+class FileBrowser():
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def browseFile():
+        currdir = os.getcwd()  # current python directory when open filedialog
+        filename = filedialog.askopenfilename(initialdir=currdir, title='Please select an image', filetypes=[
+            ("image", ".jpeg"),
+            ("image", ".png"),
+            ("image", ".jpg"),
+        ])
+        return filename
